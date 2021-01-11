d83559c0742a682b862b77251d93069cf8efa710
Eslam Magdy <eslam3400@gmail.com>
2021-01-11 17:51:39 +0200

    bind style width to more dinamic visuals

--- app.js
+++ app.js
@@ -11,30 +11,18 @@ const game = Vue.createApp({
             playerName: null,
             playerAttDmg: Math.floor(Math.random() * 20),
             playerHealth: 100,
-            playerHealthBar: "<div class='progress-bar progress-bar-striped progress-bar-animated bg-success' \
-                role='progressbar' style='width:100%' aria-valuenow='100' aria-valuemin='0' \
-                aria-valuemax='100'>100</div>",
             monsterAttDmg: Math.floor(Math.random() * 15),
-            monsterHealth: 100,
-            monsterHealthBar: "<div class='progress-bar progress-bar-striped progress-bar-animated bg-success' \
-                role='progressbar' style='width:100%' aria-valuenow='100' aria-valuemin='0' \
-                aria-valuemax='100'>100</div>"
+            monsterHealth: 100
         }
     },
     watch: {
         playerHealth() {
-            this.playerHealthBar = "<div class='progress-bar progress-bar-striped progress-bar-animated bg-success' \
-                role='progressbar' style='width:" + this.playerHealth + "%' aria-valuenow='" + this.playerHealth + "' aria-valuemin='0' \
-                aria-valuemax='100'>" + this.playerHealth + "</div>"
             if (this.playerHealth <= 0) {
                 alert('You lost :(')
                 this.resetGame()
             }
         },
         monsterHealth() {
-            this.monsterHealthBar = "<div class='progress-bar progress-bar-striped progress-bar-animated bg-success' \
-                role='progressbar' style='width:" + this.monsterHealth + "%' aria-valuenow='" + this.monsterHealth + "' aria-valuemin='0' \
-                aria-valuemax='100'>" + this.monsterHealth + "</div>"
             if (this.monsterHealth <= 0) {
                 alert('You won :D')
                 this.resetGame()
@@ -53,19 +41,13 @@ const game = Vue.createApp({
             this.playerName = null
             this.playerAttDmg = Math.floor(Math.random() * 20)
             this.playerHealth = 100
-            this.playerHealthBar = "<div class='progress-bar progress-bar-striped progress-bar-animated bg-success' \
-                role='progressbar' style='width:100%' aria-valuenow='100' aria-valuemin='0' \
-                aria-valuemax='100'>100</div>"
             this.monsterAttDmg = Math.floor(Math.random() * 15)
             this.monsterHealth = 100
-            this.monsterHealthBar = "<div class='progress-bar progress-bar-striped progress-bar-animated bg-success' \
-                role='progressbar' style='width:100%' aria-valuenow='100' aria-valuemin='0' \
-                aria-valuemax='100'>100</div>"
         },
     },
-        computed: {
-            getPlayerName() {
-                this.playerName = sessionStorage.getItem('sonic__playerName')
-            }
+    computed: {
+        getPlayerName() {
+            this.playerName = sessionStorage.getItem('sonic__playerName')
         }
-    }).mount('#appContainer__app')
+    }
+}).mount('#appContainer__app')
