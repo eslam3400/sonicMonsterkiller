cfe4da756f78b88b6212233d0b586683641fa58f
Eslam Magdy <eslam3400@gmail.com>
2021-01-12 01:25:54 +0200

    added energy and specail moves

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ const game = Vue.createApp({
         return {
             playerName: null,
             playerHealth: 100,
+            playerEnergy: 0, 
             monsterHealth: 100,
+            monsterEnergy: 0,
             battelStatus: null,
             battelLog: []
         }
@@ -27,22 +29,55 @@ const game = Vue.createApp({
                 alert('You won :D')
                 this.battelStatus = 'ended'
             }
+        },
+        monsterEnergy(){
+            if (this.monsterEnergy >= 100) {
+                let randomChoies = Math.floor(Math.random() * 2)
+                if( randomChoies == 0 ) this.monsterHeal()
+                else this.monsterSpecialAtt()
+            }
         }
     },
     methods: {
         attackMonster() {
-            let dmg = Math.floor(Math.random() * 20)
+            let dmg = Math.floor(Math.random() * 10)
             this.monsterHealth -= dmg
             this.addBattelLog(sessionStorage.getItem('sonic__playerName'),'attacked',dmg)
-            this.attackplayer()
+            if (this.monsterHealth > 0) this.attackplayer()
+            this.playerEnergy += Math.floor(Math.random() * 20)
         },
         attackplayer() {
-            let dmg = Math.floor(Math.random() * 20)
-            this.addBattelLog('Monster','attacked',dmg)
+            let dmg = Math.floor(Math.random() * 10)
+            this.addBattelLog('Monster','Attacked',dmg)
             this.playerHealth -= dmg
+            this.monsterEnergy += Math.floor(Math.random() * 20)
         },
         addBattelLog(who,what,amount){
             this.battelLog.push({who,what,amount})
+        },
+        playerHeal(){
+            let heal = Math.floor(Math.random() * 15)
+            this.playerHealth += heal
+            this.addBattelLog(sessionStorage.getItem('sonic__playerName'),'Healed',heal)
+            this.playerEnergy = 0
+        },
+        monsterHeal(){
+            let heal = Math.floor(Math.random() * 15)
+            this.monsterHealth += heal
+            this.addBattelLog('Monster','Healed',heal)
+            this.monsterEnergy = 0
+        },
+        playerSpecialAtt(){
+            let specialAttack = Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10)
+            this.monsterHealth -= specialAttack
+            this.addBattelLog(sessionStorage.getItem('sonic__playerName'),'Special Attack',specialAttack)
+            this.playerEnergy = 0
+        },
+        monsterSpecialAtt(){
+            let specialAttack = Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10)
+            this.playerHealth -= specialAttack
+            this.addBattelLog('Monster','Special Attack',specialAttack)
+            this.monsterEnergy = 0
         },
         resetGame() {
             this.playerName = null
@@ -50,11 +85,16 @@ const game = Vue.createApp({
             this.monsterHealth = 100
             this.battelLog = []
             this.battelStatus = null
+            this.playerEnergy =  0 
+            this.monsterEnergy =  0
         }
     },
     computed: {
         playerHealthBar(){
             return {width: this.playerHealth + "%"}
+        },
+        playerEnergyBar(){
+            return {width: this.playerEnergy + "%"}
         },
         playerHealthBarColor(){
             if (this.playerHealth <= 20) return {'bg-danger':true}
@@ -63,6 +103,9 @@ const game = Vue.createApp({
         },
         monsterHealthBar(){
             return {width: this.monsterHealth + "%"}
+        },
+        monsterEnergyBar(){
+            return {width: this.monsterEnergy + "%"}
         },
         monsterHealthBarColor(){
             if (this.monsterHealth <= 20) return {'bg-danger':true}
